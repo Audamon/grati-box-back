439168371444bc7a145015857d581f474d721004
Audamon <joaotamagno@gmail.com>
2021-11-22 14:45:37 -0300

    Subscription and End order was implemented

--- src/Controllers/signup.js
+++ src/Controllers/signup.js
@@ -1,17 +1,18 @@
 import bcrypt from 'bcrypt';
-import connection from '../database';
-import { signUpSchema } from '../../Schemas/schemas';
+import connection from '../database.js';
+import { signUpSchema } from '../../Schemas/schemas.js';
 
 async function signup(req, res) {
   const validation = signUpSchema.validate(req.body);
   if (validation.error) {
     return res.sendStatus(400);
   }
+  console.log(1);
   const { name, email, password } = req.body;
   const encryptedPassword = bcrypt.hashSync(password, 10);
   try {
-    await connection.query('INSERT INTO "user"(name, email, password) VALUES($1, $2, $3)', [name, email, encryptedPassword]);
-    return res.sendStatus(200);
+    const t = await connection.query('INSERT INTO "user"(name, email, password) VALUES($1, $2, $3)', [name, email, encryptedPassword]);
+    return res.status(200).send(t.rows);
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+/* eslint-disable prefer-template */
+/* eslint-disable quotes */
+import './setup.js';
 import app from './app.js';
 
-app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT);
-});
+app.listen(process.env.PORT);
